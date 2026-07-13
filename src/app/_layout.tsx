ee92098774b1877a28f33d7f92bb2dd5f8b5a956
cHANGTEEZY <jagatgurung30@gmail.com>
2026-07-13 17:59:08 +0545

    add hero ui with uniwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,25 @@
+import "../global.css";
+
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { HeroUINativeProvider } from "heroui-native";
 
 import { AppProviders } from "@/providers/app-providers";
 
 export default function RootLayout() {
   return (
-    <AppProviders>
-      <StatusBar style="auto" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(app)" />
-      </Stack>
-    </AppProviders>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <AppProviders>
+          <StatusBar style="auto" />
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(app)" />
+          </Stack>
+        </AppProviders>
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
   );
 }
